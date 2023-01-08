7d8140a6160d0d72bbab53d1ed97582e620a01cf
YanaySella <yanaysella@gmail.com>
2023-01-08 16:46:41 +0200

    fix link wrapping

--- itsaviya/src/home/components/navbar/NavBar.jsx
+++ itsaviya/src/home/components/navbar/NavBar.jsx
@@ -5,14 +5,14 @@ import Logo from "./../../images/aviyaLogo.png";
 const NavBar = () => {
   return (
     <div className="flex items-center self-stretch bg-secondary h-28 justify-around px-20 fixed top-0 left-0 right-0 z-10 gap-5">
-      <div className="flex flex-wrap gap-4 basis-60">
+      <div className="flex flex-wrap md:gap-14 gap-5">
         <NavBtn text="About" to="#about" size="lg" />
         <NavBtn text="Plans" to="#plans" size="lg" />
       </div>
 
       <img src={Logo} alt="" className="h-28 w-28 justify-self-start" />
 
-      <div className="flex flex-wrap gap-4 basis-60 justify-end">
+      <div className="flex flex-wrap md:gap-14 gap-5 justify-end">
         <NavBtn text="Blog" to="#" size="lg" />
         <NavBtn text="Contact" to="#" size="lg" />
       </div>
