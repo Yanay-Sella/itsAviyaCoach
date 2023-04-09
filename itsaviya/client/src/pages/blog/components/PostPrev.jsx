cb8d255410810ddcde0d19a9179b550068da5791
Yanay-Sella <Yanaysella@gmail.com>
2023-04-09 11:59:41 +0300

    fixed style on phone

--- itsaviya/client/src/pages/blog/components/PostPrev.jsx
+++ itsaviya/client/src/pages/blog/components/PostPrev.jsx
@@ -9,7 +9,9 @@ const PostPrev = ({ blogSummary }) => {
     <Link to={`/blog/${name}`}>
       <section className="md:pl-10 transition-all flex md:flex-row flex-col md:justify-end md:gap-16 gap-4 border-2 border-fourthy rounded-lg hover:-translate-y-1 hover:cursor-pointer shadow-md hover:shadow-xl">
         <div className="flex flex-col gap-4 md:items-stretch items-center order-1 md:order-2">
-          <h1 className="header text-3xl md:text-right text-center">{title}</h1>
+          <h1 className="header md:text-3xl text-xl md:text-right text-center">
+            {title}
+          </h1>
           <div className="hidden md:inline">
             <p className="text-xl break-words">{intro}</p>
           </div>
