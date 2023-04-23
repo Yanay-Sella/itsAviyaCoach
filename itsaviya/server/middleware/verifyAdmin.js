22760b93c0b8d90beca3ad7d4db22e9a9ea86c2c
Yanay-Sella <Yanaysella@gmail.com>
2023-04-23 16:54:59 +0300

    github safe

--- itsaviya/server/middleware/verifyAdmin.js
+++ itsaviya/server/middleware/verifyAdmin.js
@@ -1,12 +1,3 @@
-// const verifyAdmin = (req, res, next) => {
-//   if (!req.role) return res.sendStatus(401);
-//   const allowed = req.role === 2018;
-//   if (!allowed) return res.sendStatus(401);
-//   next();
-// };
-
-// module.exports = verifyAdmin;
-
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
