667d7c76758996e5fafcd5360dfbdfdf7bd9b3e3
Yanay-Sella <Yanaysella@gmail.com>
2023-05-13 13:06:31 +0300

    fixush

--- itsaviya/client/src/api/axios.js
+++ itsaviya/client/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL = process.env.REACT_APP_SERVER_URL;
 
 export default axios.create({
-  baseURL: BASE_URL,
+  baseURL: BASE_URL || "",
   headers: { "Content-Type": "application/json" },
   validateStatus: () => true,
 });
